9aad31f5af65badadfde859da1ea2eda0898977e
Joaquin-Huaranga <joaquinhuarangaorihuela@gmail.com>
2024-11-05 18:00:36 -0500

    add geolocation user to Map (#9)

--- packages/hosting/src/pages/home/mapComponents/Map.jsx
+++ packages/hosting/src/pages/home/mapComponents/Map.jsx
@@ -1,33 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 import { mapOptions } from "./MapConfiguration.jsx";
 
 export const Map = (props) => {
   const { isLoaded } = props;
 
+  const [userLocation, serUserLocation] = useState(null);
+
   const containerStyle = {
     width: "100%",
     height: "100vh",
   };
 
-  const homeLocation = {
-    lat: -12.169521,
-    lng: -77.021054,
-  };
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          serUserLocation({ lat: latitude, lng: longitude });
+        },
+        (error) => {
+          console.error("No se acpetó la geolocalización");
+        },
+      );
+    } else {
+      console.error("Su navegador no cuenta con geolocalizacion");
+    }
+  }, []);
+
+  // const homeLocation = {
+  //   lat: -12.169521,
+  //   lng: -77.021054,
+  // };
 
   return (
     isLoaded && (
       <>
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={homeLocation}
+          center={userLocation}
           zoom={18}
           options={{
             styles: mapOptions.mapTheme,
             ...mapOptions.mapConfigurations,
           }}
         >
-          <Marker position={homeLocation} />
+          <Marker position={userLocation} />
         </GoogleMap>
       </>
     )
